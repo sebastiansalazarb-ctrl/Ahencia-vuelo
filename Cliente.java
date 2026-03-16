6ddd753e5c079f2d381df197aa84fc59163f4e5e
mariajperezgo-oss <mariaj.perezgo@autonoma.edu.co>
2026-03-16 16:06:23 -0500

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -6,19 +6,22 @@ public class Cliente {
     protected String apellido;
     protected String telefono;
     protected String cedula;
+    protected String email;
     
     
-    public Cliente(String nombre, String apellido, String telefono,String cedula ) {
+    public Cliente(String nombre, String apellido, String telefono,String cedula, String email ) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.telefono = telefono;
         this.cedula=cedula;
+        this.email = email;
     }
 
     public String getNombre(){return nombre;}
     public String getApellido(){return apellido;}
     public String getTelefono(){return telefono;}
     public String getCedula(){return cedula;}
+    public String getEmail(){return email;}
      
      
     public String getNombreCompleto() {
