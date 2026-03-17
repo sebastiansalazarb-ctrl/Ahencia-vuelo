57d9b25db3214bd9975bc8c68d406c1e60af0cab
Fabian Esteban Granada Aviles <estuam@UAM.LOCAL>
2026-03-17 18:22:49 -0500

    cree el constructor de vuelo

--- Vuelo.java
+++ Vuelo.java
@@ -1,4 +1,20 @@
 public class Vuelo{
 
+    protected String aerolinea;
+    protected String destino;
+    protected String origen;
+    protected String vuelocol;
+    protected String fecha;
+    protected String sillas;
+    
+    public Cliente(String aerolinea, String destino, String origen,String vuelocol , String fecha, String sillas) {
+        this.aerolinea = aerolinea;
+        this.destino = destino;
+        this.origen = origen;
+        this.vuelocol = vuelocol;
+        this.fecha = fecha;
+        this.sillas = sillas;
+    }
+
   
 }
