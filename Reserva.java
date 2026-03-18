e56d4be533e4120b72825df3e77f2fd4ba9a6fc5
Estudiante <estuam@UAM.LOCAL>
2026-03-17 20:27:40 -0500

    agrege clente,fecha y persona en javaa

--- Reserva.java
+++ Reserva.java
@@ -1,5 +1,17 @@
-public class Reserva{
+public class Reserva {
+    private String cliente;
+    private String fecha;
+    private int personas;
 
+    public Reserva(String cliente, String fecha, int personas) {
+        this.cliente = cliente;
+        this.fecha = fecha;
+        this.personas = personas;
+    }
 
-  
+    public void mostrarReserva() {
+        System.out.println("Reserva a nombre de: " + cliente);
+        System.out.println("Fecha: " + fecha);
+        System.out.println("Personas: " + personas);
+    }
 }
