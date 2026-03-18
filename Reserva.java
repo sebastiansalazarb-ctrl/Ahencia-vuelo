8b7d0dda8c94991598e0b4d2040acc04cfaa2b95
Juan <tu_correo@gmail.com>
2026-03-17 19:55:27 -0500

    Añadida mejora por Juan Pablo

--- Reserva.java
+++ Reserva.java
@@ -1,5 +1,23 @@
 public class Reserva{
+    //JUAN PABLO HOYOS
+    int id;
+    String usuario;
+    String vuelo;
+    String hotel;
 
+    void crear(int id, String usuario, String vuelo, String hotel) {
+        this.id = id;
+        this.usuario = usuario;
+        this.vuelo = vuelo;
+        this.hotel = hotel;
+    }
 
-  
+        public void mostrarReserva() {
+        System.out.println("ID: " + id);
+        System.out.println("Usuario: " + usuario);
+        System.out.println("Vuelo: " + vuelo);
+        System.out.println("Hotel: " + hotel);
+    }
+    // JUAN P H 
+
 }
