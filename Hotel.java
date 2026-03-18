e907feef28cbe4d820187ab9630420538f3b53e7
sergioaceballose <sergioa.ceballose@autonoma.edu.co>
2026-03-18 00:08:17 +0000

    Agregue un metodo

--- Hotel.java
+++ Hotel.java
@@ -6,4 +6,13 @@ public class Hotel{
     public String direccion;
     public int habitaciones;
 
+    public void mostrarDatos() {
+ System.out.println("Hotel: " + nombre);
+ System.out.println("Hotel: " + telefono);
+ System.out.println("Hotel: " + correo);
+ System.out.println("Hotel: " + direccion);
+ System.out.println("Hotel: " + habitaciones);
+ }
+
+
 }
